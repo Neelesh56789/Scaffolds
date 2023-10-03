24ee3e07dbdca2e99c9aac492a7183c0a6f006f6
Neelesh <neeleshtiwari00776@gmail.com>
2023-10-03 10:01:38 +0530

    second-comit

---
+++ Spaceship/script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const hitSound = document.getElementById("hitSound");
+const groundSound = document.getElementById("groundSound");
+
+//Write the javascript here
