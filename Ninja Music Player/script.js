a44e2aff3e50064549e63ecc8e88600c063ac2ce
Neelesh <neeleshtiwari00776@gmail.com>
2023-10-04 12:10:12 +0530

    third-commit

--- Ninja Music Player/script.js
+++ Ninja Music Player/script.js
@@ -1,1 +1,25 @@
-// Write the javascript here
+// Write your javascript here
+
+    tracks = [
+        {
+            name: "Let me down slowly",
+            artist: "Alec Benjamin",
+            cover: "alec.jpg",
+            source: "Let me down slowly.mp3",
+        },
+        {
+            name: "Let me love you",
+            artist: "DJ Snake/Justin Beiber",
+            cover: "dj.jpg",
+            source: "Let me love you.mp3",
+        },
+        {
+            name: "Perfect",
+            artist: "Ed Sheeran",
+            cover: "ed.jpg",
+            source: "Perfect.mp3",
+        },
+        
+    ];
+
+
